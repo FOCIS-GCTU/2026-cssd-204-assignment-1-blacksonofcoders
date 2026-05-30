515fb8e64c31221a6a9aa7b55fca37461b7a5d05
blacksonofcoders <2425400662@live.gctu.edu.gh>
2026-05-30 19:44:19 +0000

    CTT.PY ASSIGNMENT 1

--- initials.py
+++ initials.py
@@ -2,6 +2,40 @@
 def main():
   pass #code goes here
 
+# File: CTT.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Cornelius Tetteh Tetteh
+# STUDENT ID: 2425400662
+#GROUP :A
+# Email: 2425400662@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor,Cornelius Tetteh Tetteh, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+def main():
+    # Prints my initials CTT in large stylized letters.
+
+    print()
+
+    print("...CTT")
+
+    print()
+
+    print("...CCCCCCCCCCCC........TTTTTTTTTTTT........TTTTTTTTTTTT.....")
+    print("...CCCCCCCCCCCC........TTTTTTTTTTTT........TTTTTTTTTTTT.....")
+    print("...CCC.....................TTTT................TTTT.........")
+    print("...CCC.....................TTTT................TTTT.........")
+    print("...CCC.....................TTTT................TTTT.........")
+    print("...CCC.....................TTTT................TTTT.........")
+    print("...CCC.....................TTTT................TTTT.........")
+    print("...CCC.....................TTTT................TTTT.........")
+    print("...CCCCCCCCCCCC...**.......TTTT.......**.......TTTT.......**")
+    print("...CCCCCCCCCCCC...**.......TTTT.......**.......TTTT.......**")
+
+    print()
 
 
 main()
